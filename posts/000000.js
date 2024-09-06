b50d4dfde326b4b3fcf0732d4c1797ea310d2dbe
Nathan Hering <nathanh@gointerject.com>
2024-09-05 20:16:53 -0400

    update template

--- posts/000000.js
+++ posts/000000.js
@@ -1,13 +1,14 @@
-// NOTE: Do not add the dot to the begining of the source paths
 let slides_000000 = JSON.stringify([
-    ['/images/2022/04/.jpg','caption']
+    ['/images/2023/06/.jpg','caption']
 ])
 
 content.innerHTML = `\
-    <h1></h1>\
-    <h5>dddd mmmm dd yyyy</h5>\
+    <h1>Title</h1>\
+    <h5>dddd mmm dd yyyy</h5>\
     <br>\
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lorem mauris.</p>\
+    <p>Intro</p>\
+    <br>\
+    <p>Content</p>\
     <br>\
     <div class='slides' data-sources='${slides_000000}'></div>
 `
